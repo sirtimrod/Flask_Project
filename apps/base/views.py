ebb8883ce82d5159c1134b2b1c5c2a6fd8a833ad
sirtimrod <rodyamilk@gmail.com>
2021-10-15 14:37:11 +0300

    Changed get view

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,8 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 from flask.views import MethodView
+
+from apps.base.models import Page
 
 
 class HomeView(MethodView):
 
     def get(self):
-        return render_template('base.html')
+        user_ip = request.remote_addr
+        new_note = Page.add_note(str(user_ip))
+        return render_template('base.html', info=new_note)
