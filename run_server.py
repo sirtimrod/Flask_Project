242a6cb38ec56c5eff3c1ef6824aa16a6bae2734
sirtimrod <rodyamilk@gmail.com>
2021-10-15 14:36:50 +0300

    Changed run file

--- run_server.py
+++ run_server.py
@@ -4,5 +4,14 @@ from app import install_app
 app = install_app()
 
 
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+
+    """This function closing the session"""
+
+    from db import session
+    session.remove()
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
