7944ad9dec2d9b3ad7ae8aac77622fbac0e63036
sirtimrod <rodyamilk@gmail.com>
2021-10-20 14:37:26 +0300

    Changes in config

--- config.py
+++ config.py
@@ -4,5 +4,9 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///base.db')
+
+if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
