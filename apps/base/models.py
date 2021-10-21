f8ad10ef62c571516b4417e62c8440f1ca6fe265
sirtimrod <rodyamilk@gmail.com>
2021-10-21 16:17:24 +0300

    Changed model

--- apps/base/models.py
+++ apps/base/models.py
@@ -8,13 +8,17 @@ class Page(Base):
     __tablename__: str = 'pages'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    ip = sq.Column(sq.String)
+    internal_ip = sq.Column(sq.String)
+    external_ip = sq.Column(sq.String)
+    country_code = sq.Column(sq.String)
     created_at = sq.Column(sq.DateTime, server_default=func.now())
 
     @classmethod
-    def add_note(cls, user_ip: str):
+    def add_note(cls, user_ip: str, external_ip: str, country_code: str):
         new_note = Page()
-        new_note.ip = user_ip
+        new_note.internal_ip = user_ip
+        new_note.external_ip = external_ip
+        new_note.country_code = country_code
         session.add(new_note)
         session.commit()
         return new_note
--- apps/base/views.py
+++ apps/base/views.py
@@ -1,14 +1,22 @@
 from flask import render_template, request
 from flask.views import MethodView
 
+import requests
+
 from apps.base.models import Page
 
 
 class HomeView(MethodView):
 
     def get(self):
+
         user_ip = request.remote_addr
-        Page.add_note(str(user_ip))
+
+        external_ip = requests.get('https://api.ipify.org').text
+        endpoint = f'https://ipinfo.io/{external_ip}/json'
+        country_code = requests.get(endpoint, verify=True).json()
+
+        Page.add_note(user_ip, external_ip, country_code['country'])
         get_all = Page.get_all()
         items = []
         for i in get_all:
