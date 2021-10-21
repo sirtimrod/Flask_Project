a533475193e6823b70ac8b781eb7cdcb9c035528
sirtimrod <rodyamilk@gmail.com>
2021-10-21 16:29:40 +0300

    Deleted migrations

--- migrations/env.py
+++ migrations/env.py
@@ -51,7 +51,7 @@ def run_migrations_offline():
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        render_as_batch=True
+        # render_as_batch=True
     )
 
     with context.begin_transaction():
@@ -73,7 +73,8 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata, render_as_batch=True
+            connection=connection, target_metadata=target_metadata,
+            # render_as_batch=True
         )
 
         with context.begin_transaction():
