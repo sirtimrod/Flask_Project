5b83191b9f7f0172da4094e823083f02bb7a4a4d
sirtimrod <rodyamilk@gmail.com>
2021-10-22 11:15:23 +0300

    Added requirements

--- apps/base/views.py
+++ apps/base/views.py
@@ -12,11 +12,12 @@ class HomeView(MethodView):
 
         user_ip = request.remote_addr
 
-        external_ip = requests.get('https://api.ipify.org').text
-        endpoint = f'https://ipinfo.io/{external_ip}/json'
-        country_code = requests.get(endpoint, verify=True).json()
+        # external_ip = requests.get('https://api.ipify.org').text
+        user_data = requests.get(f'https://ipinfo.io/?ip={user_ip}').json()
+        # country_code = requests.get(endpoint, verify=True).json()
+        print(user_data)
 
-        Page.add_note(user_ip, external_ip, country_code['country'])
+        Page.add_note(user_ip, user_data['ip'], user_data['country'])
         get_all = Page.get_all()
         items = []
         for i in get_all:
