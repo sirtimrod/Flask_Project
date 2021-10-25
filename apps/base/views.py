1f64fdde3f6e7e9ea85c8352de34cecf410fcb8e
sirtimrod <rodyamilk@gmail.com>
2021-10-25 16:42:34 +0300

    Changed view method

--- apps/base/views.py
+++ apps/base/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 from flask.views import MethodView
 
 import requests
+from bs4 import BeautifulSoup
 
 from apps.base.models import Page
 
@@ -13,17 +14,15 @@ class HomeView(MethodView):
         user_data = request
         print(vars(user_data))
         external_ip = requests.get('https://api64.ipify.org/').text
+        country_code = requests.get(f'http://api.ipstack.com/{external_ip}?'
+                                    f'access_key=74a3ef2e0e526908b52d42f40bd961ef').json()
 
-        if 'HTTP_X_FORWARDED_FOR' in vars(user_data)['environ']:
+        if 'HTTP_X_FORWARDED_FOR' in user_data.environ:
             external_ip = user_data.environ['HTTP_X_FORWARDED_FOR']
-            print('External IP inside IF:', external_ip)
+            country_code = requests.get(f'http://api.ipstack.com/{external_ip}?'
+                                        f'access_key=74a3ef2e0e526908b52d42f40bd961ef').json()
 
-        country_code = 'RU'
-
-        print('Internal IP outside IF:', user_data.remote_addr)
-        print('External IP outside IF:', external_ip)
-
-        Page.add_note(user_data.remote_addr, external_ip, country_code)
+        Page.add_note(user_data.remote_addr, external_ip, country_code['country_code'])
         get_all = Page.get_all()
         items = []
         for i in get_all:
