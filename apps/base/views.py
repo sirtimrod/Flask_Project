11fe47d7888b66097cc7843ad1a2730db34e2b44
sirtimrod <rodyamilk@gmail.com>
2021-11-01 10:44:10 +0300

    Fixed view method

--- apps/base/views.py
+++ apps/base/views.py
@@ -18,7 +18,7 @@ class HomeView(MethodView):
 
         if 'HTTP_X_FORWARDED_FOR' in user_data.environ:
             external_ip = user_data.environ['HTTP_X_FORWARDED_FOR']
-            country_code = scripts(external_ip)
+            country_code = scripts.get_country_code(external_ip)
 
         Page.add_note(user_data.remote_addr, external_ip, country_code)
         get_all = Page.get_all()
